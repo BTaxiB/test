43ff6c8bf255f8cbecdc93847601f0b754f6b42d
unknown <joksimovicmiki7@gmail.com>
2021-01-11 20:42:13 +0100

    minor changes

--- src/traits/Sanitation.php
+++ src/traits/Sanitation.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 trait Sanitation
 {
 
-    function validInput(string $data): string
+    static function validInput(string $data): string
     {
         $data = trim($data);
         $data = stripslashes($data);
