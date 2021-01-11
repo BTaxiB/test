0a8f14e0a3283fcbb48a8cbe563c3c2f1812f579
unknown <joksimovicmiki7@gmail.com>
2021-01-11 21:14:36 +0100

    removed type hint for validInput

--- src/traits/Sanitation.php
+++ src/traits/Sanitation.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 trait Sanitation
 {
 
-    static function validInput(string $data): string
+    static function validInput($data)
     {
         $data = trim($data);
         $data = stripslashes($data);
