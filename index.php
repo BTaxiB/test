d05db7f85d3d2f024310a4e4dcc6c1f3fc2ccd32
unknown <joksimovicmiki7@gmail.com>
2021-01-12 11:14:07 +0100

    switch get request

--- index.php
+++ index.php
@@ -2,6 +2,23 @@
 
 session_start();
 
-if(!isset($_SESSION['user_id'])) {
-    require_once 'views/user/login.php';
+
+switch ($_GET) {
+    case isset($_GET['signed_in']):
+        $_SESSION['user_id'] = $_GET['code'];
+        break;
+    case isset($_GET['login_err']):
+        
+        break;
+
+    case isset($_GET['logout']):
+        session_start();
+        session_destroy();
+        break;
+
+    default:
+        if (!isset($_SESSION['user_id'])) {
+            require_once 'views/user/login.php';
+        }
+        break;
 }
